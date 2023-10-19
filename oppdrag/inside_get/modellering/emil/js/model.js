7fc668a19889c59de479b047f0c005d35d8b3cac
MeatMahjong <markus.m.eliassen@gmail.com>
2023-10-19 14:57:35 +0200

    Jobbet litt vidre med modellen

--- oppdrag/inside_get/modellering/emil/js/model.js
+++ oppdrag/inside_get/modellering/emil/js/model.js
@@ -28,10 +28,8 @@ const model = {
 
     /*
      * Oppbygning av lære-innhold
-     *     Et kurs består av flere emner
-     *     Et emne består av flere tema
-     *     Et tema består av flere artikler
-     *     En artikkel består av flere atomer
+     *     En uke har en artikkel per dag
+     *     En artikkel tilhører et tema og består av flere atomer
      *     Et atom er enten et Bilde, en tekst, en kodesnutt, en video eller noe annet..
      *
      * Eksempel:
@@ -40,18 +38,16 @@ const model = {
      *     Atom     -> <En video som forklarer pomodoro teknikken>
      */
     ukeplan: [
-        { uke: 1, artikler: {mandag: 4, tirsdag: 1, onsdag: 5, /*etc*/} },
+        { uke: 1, artikler: {mandag: [4, 3], tirsdag: 1, onsdag: 5, /*etc*/} },
         { uke: 2, artikler: {mandag: 5, tirsdag: 2, onsdag: 6, /*etc*/} },
+        { uke: 10, artikler: {mandag: 5, tirsdag: 2, onsdag: 6, torsdag: 12, /*etc*/} },
     ],
-    hovedtema: [ // flere artikler under et tema, legg til disse i artikler: []
+    tema: [ // flere artikler under et tema, legg til disse i artikler: []
         { id: 1, name: 'Tekning og læring',   },
         { id: 2, name: 'Samarbeid og kommunikasjon' },
         { id: 3, name: 'Selvledelse og refleksjon' },
     ],
-    undertema: [
-        { hovedtema: 1, undertema: 'Kritisk tenkning', artikler: [1,2], },
-        { hovedtema: 1, undertema: 'Focused vs diffused' },
-    ],
+
     nøkkelord: [ // nøkkelord sortering
         { id: 1, navn: "Tema" },
         { id: 1, navn: "Psykologi" },
@@ -70,9 +66,9 @@ const model = {
     ],
     artikler: [ // flere atomer under en artikkel, legg til disse i atomer: []
          { id: 1, name: 'Pomodoro',  atomer: [1, 3, 2, 4, 5], nøkkelord: [2], temaId: 1 },
-         { id: 2, name: 'Læring',    atomer: [6, 12, 7, 13], nøkkelord: [], datoer: ['YYYY-MM-DD', 'YYYY-MM-DD'],  },
-         { id: 3, name: 'Feiling',   atomer: [11, 12], nøkkelord: [3] },
-         { id: 4, name: 'Samarbeid', atomer: [8, 9, 10], nøkkelord: []  },
+         { id: 2, name: 'Læring',    atomer: [6, 12, 7, 13], nøkkelord: [], temaId: 2  },
+         { id: 3, name: 'Feiling',   atomer: [11, 12], nøkkelord: [3], temaId: 1 },
+         { id: 4, name: 'Samarbeid', atomer: [8, 9, 10], nøkkelord: [], temaId: 3   },
     ],
     atomer: [ // hvert atom har innhold som er definert innenfor en spesifikk type (for å generere riktig html), innhold legges i atom: {}
        { id: 1,  type: 'text',       atom: { title: 'Aktiv-lytting', text: 'For løkker er viktig i mange situasjoner. Bla bla..' } },
@@ -108,47 +104,45 @@ Derav behovet for ulike innganger til hvert tema.
     4 via relaterte tema som i pomodoro har med både focused og diffused mindset og læring - Nøkkelord
     5 et slags type innhold - eksempel: pomodoro som er teknikk - Nøkkelord
 
-    Nøkkelkompetanse:
-  # HOVED-TEMA
-  Tenkning og læring:
-    #  UNDER-TEMA
-    - Problemløsning
-      # - Artikkel 1
-      # - Artikkel 2
-      # - .....
-    - Læring
-    - Kritisk tenkning
-    - Pomodoro
-    - Focused vs diffused
-    - Show and tell
-    - Feynman-teknikken
-    - Feiling
-  Samarbeid og kommunikasjon:
-    - Kommunikasjon
-    - Aktiv lytting
-    - Samarbeid
-    - Sosial kompetanse
-    - Tuckmans modell rundt teamdynamikk
-    - Psykologisk trygghet
-    - Googles Project Aristotle
-  Selvledelse og refleksjon:
-    - Attribusjon
-    - Daglig logg
-    - Egenmotivasjon og prokrastinering
-    - Emosjonell kompetanse
-    - Etisk refleksjon
-    - GRIT
-    - Gode vaner
-    - Growth mindset
-    - Learned helplessness
-    - Locus of control
-    - Mestringstro
-    - Metakognisjon
-    - Personlighetspsykologi
-    - Signaturstyrker
-    - 7 Habits
-    - Isfjellmetafor
-    - Nevroplastisitet
-    - The Art of Failing Well
 
+  Nøkkelkompetanse oppbygning:
+  # TEMA - Tenkning og læring:
+    # ARTIKLER
+        - Problemløsning
+        - Læring
+        - Kritisk tenkning
+        - Pomodoro
+        - Focused vs diffused learning
+        - Show and tell
+        - Feynman-teknikken
+        - Feiling
+  # TEMA - Samarbeid og kommunikasjon:
+    # ARTIKLER
+        - Kommunikasjon
+        - Aktiv lytting
+        - Samarbeid
+        - Sosial kompetanse
+        - Tuckmans modell rundt teamdynamikk
+        - Psykologisk trygghet
+        - Googles Project Aristotle
+  # TEMA - Selvledelse og refleksjon:
+    # ARTIKLER
+        - Attribusjon
+        - Daglig logg
+        - Egenmotivasjon og prokrastinering
+        - Emosjonell kompetanse
+        - Etisk refleksjon
+        - GRIT
+        - Gode vaner
+        - Growth mindset
+        - Learned helplessness
+        - Locus of control
+        - Mestringstro
+        - Metakognisjon
+        - Personlighetspsykologi
+        - Signaturstyrker
+        - 7 Habits
+        - Isfjellmetafor
+        - Nevroplastisitet
+        - The Art of Failing Well
 */
