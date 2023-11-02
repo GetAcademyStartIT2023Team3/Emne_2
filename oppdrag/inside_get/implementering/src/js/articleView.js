532c3e8ebe707a7ff76d75146913bd9a61e1046e
Emil Bratt <emilb@getacademy.no>
2023-11-02 13:53:54 +0100

    CSS for articleView
    Legger til CSS-regler for articleView.js.
    Lager en ny css fil for denne -> oppdrag/inside_get/implementering/src/css/article.css
    Oppdaterer html-filen til å hente inn denne -> oppdrag/inside_get/implementering/src/index.html

--- oppdrag/inside_get/implementering/src/js/articleView.js
+++ oppdrag/inside_get/implementering/src/js/articleView.js
@@ -1,9 +1,11 @@
 function updateArticleView() {
-    let html = '<div class="container">';
+    let html = '';
+    html += '<div id="articlePage">';
 
     html += renderNotes();
     html += renderArticle();
-    html += renderFooter();
+    html += renderNavigation();
+    
     html += '</div>';
 
     return html;
@@ -11,9 +13,11 @@ function updateArticleView() {
 
 function renderNotes() {
     let html = /*html*/`
-        <div class="container">
-            <h2>Notater</h2>
-            <textarea class="article-notes"></textarea>
+        <div id="articlePageNotes">
+            <div style="font-size: 2rem;">
+                Notater
+            </div>
+            <textarea style="width: 90%;">Notater skrives her</textarea>
         </div>
     `;
     return html;
@@ -21,59 +25,50 @@ function renderNotes() {
 
 function renderArticle() {
     let html = /*html*/`
-        <div class="container">
-                <h1>Pomodoro</h1>
-            <div class="item-2-content">
-                <p class="content-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-                <br>
-                <p class="content-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <div id="articlePageArticle">
+            <div id="articlePageArticleView">
+                <h2>Title</h2>
+                <p>
+                    Text<br>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
             </div>
-            </div>
-                <div class="item-3">
-                    <div class="item item-3-1">
-                        <div class="item-3-1_header">
-                            <h5>Sammarbeid og kommunikasjon</h5>
-                        </div>
-                        <div class="item-3-1_content">
-                            <ul>
-                                <li><a href="">Problemløsning</a></li>
-                                <li><a href="">Læring</a></li>
-                                <li><a href="">Pomodoro</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="item item-3-2">
-                        <div class="item-3-2_header">
-                            <h5>Relaterte Artikler</h5>
-                        </div>
-                        <div class="item-3-2_content">
-                            <ul>
-                                <li><a href="">Focused/diffused mindset</a></li>
-                                <li><a href="">Pomodoro</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-    return html;
-}
-
-function renderFooter() {
-    let html = /*html*/`
-            <div class="container-3">
-                <div class="footer">
-                    <button>◀</button>
+            <div>
+                <button style="display: inline;">◀</button>
                     <div class="text">1</div>
                     <div class="text">2</div>
                     <div class="text">3</div>
-                    <button>▶</button>
-                </div>
+                <button>▶</button>
             </div>
+        </div>  
+    `;
+    return html;
+}
+
+function renderNavigation() {
+    let html = /*html*/`
+        <div id="articlePageNavigation">
+            <div>
+                <h5>Sammarbeid og kommunikasjon</h5>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="">Problemløsning</a></li>
+                    <li><a href="">Læring</a></li>
+                    <li><a href="">Pomodoro</a></li>
+                </ul>
+            </div>
+
+            <div>
+                <h5>Relaterte Artikler</h5>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="">Focused/diffused mindset</a></li>
+                    <li><a href="">Pomodoro</a></li>
+                </ul>
+            </div>
+        </div>
     `;
     return html;
 }
