cafb23ed28cd2268732b3167e3edec4567341457
Chinzcode <chinzcoding@gmail.com>
2023-11-02 10:49:25 +0100

    Jobbet med weeklySchedule HTML

--- oppdrag/inside_get/implementering/src/js/weeklyScheduleView.js
+++ oppdrag/inside_get/implementering/src/js/weeklyScheduleView.js
@@ -1,3 +1,92 @@
 function updateWeeklyScheduleView() {
-    return 'HER ER UKEPLANEN';
+    return /*HTML*/`
+    <div class="container">
+
+        <div class="content">
+            <ul>
+                <li>
+                    <h3>Mandag</h3>
+                    <a href="#">
+                        <div>Sammarbeid og kommunkasjon</div>
+                        <div>Kommunkasjon</div>
+                    </a>
+                    <a href="#">
+                        <div>XXXXX</div>
+                        <div>XXXXX</div>
+                    </a>
+                </li>
+                <li>
+                    <h3>Tirsdag</h3>
+                    <a href="#">
+                        <div>Tenknin og læring</div>
+                        <div>Nevroplastisitet</div>
+                    </a>
+                    <a href="#">
+                        <div>XXXXX</div>
+                        <div>XXXXX</div>
+                    </a>
+                </li>
+                <li>
+                    <h3>Onsdag</h3>
+                    <a href="#">
+                        <div>Sammarbeid og kommunkasjon</div>
+                        <div>Kritisk tenkning</div>
+                    </a>
+                    <a href="#">
+                        <div>XXXXX</div>
+                        <div>XXXXX</div>
+                    </a>
+                </li>
+                <li>
+                    <h3>Torsdag</h3>
+                    <a href="#">
+                        <div>Sammarbeid og kommunkasjon</div>
+                        <div>Kommunkasjon</div>
+                    </a>
+                </li>
+                <li>
+                    <h3>Fredag</h3>
+                    <a href="#">
+                        <div>Sammarbeid og kommunkasjon</div>
+                        <div>Kommunkasjon</div>
+                    </a>
+                </li>
+            </ul>
+
+            <div class="#">
+                <h2>Uke 42 - 2023:</h2>
+                <div>Uke 3 av 20</div>
+                <button>◀◀</button>
+                <button>◀</button>
+                <button>▶</button>
+                <button>▶▶</button>
+            </div>
+        </div>
+
+        <div class="sidebar">
+            <h2>Nyeste lest</h2>
+            <ul>
+                <li>
+                    <a href="#">
+                        <div>Sammarbeid og kommunkasjon</div>
+                        <div>Kommunkasjon</div>
+                    </a> 
+                </li>
+                <li>
+                    <a href="#">
+                        <div>Tenknin og læring</div>
+                        <div>Nevroplastisitet</div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <div>Sammarbeid og kommunkasjon</div>
+                        <div>Kritisk Tenkning</div>
+                    </a>
+                </li>            
+            </ul>
+        </div>
+
+    </div>
+    `;
 }
