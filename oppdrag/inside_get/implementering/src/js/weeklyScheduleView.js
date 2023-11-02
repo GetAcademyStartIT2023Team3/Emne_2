0bb8de2f66709eba9234c8096c1576e700f30872
Chinzcode <chinzcoding@gmail.com>
2023-11-02 12:31:56 +0100

    jobbet med weekly schedule html

--- oppdrag/inside_get/implementering/src/js/weeklyScheduleView.js
+++ oppdrag/inside_get/implementering/src/js/weeklyScheduleView.js
@@ -1,15 +1,9 @@
 function updateWeeklyScheduleView() {
     return /*HTML*/`
     <div class="container">
+
         <div class="weekySchedule_content">
-            <div class="weeklySchedule_navbar">
-                <h2>Uke 42 - 2023:</h2>
-                <div>Uke 3 av 20</div>
-                <button>◀◀</button>
-                <button>◀</button>
-                <button>▶</button>
-                <button>▶▶</button>
-            </div>
+
             <div class="weeklySchedule_list">
                 <ul>
                     <li>
@@ -61,7 +55,31 @@ function updateWeeklyScheduleView() {
                     </li>
                 </ul>
             </div>
+
+            <div class="weeklySchedule_#">
+                <div class="weeklySchedule_navbar">
+                    <h2>Uke 42 - 2023:</h2>
+                    <div>Uke 3 av 20</div>
+                    <button>◀◀</button>
+                    <button>◀</button>
+                    <button>▶</button>
+                    <button>▶▶</button>
+                </div>
+
+                <div class="weeklySchedule_tip">
+
+                    <div class="bubble">
+                        Dagens Tips!<br>
+                        Gjør ett eller annet.
+                    </div>
+                    <div style="position: relative">
+                        <div class="tail" style="position: absolute; left: 65px; top: -1px"></div>
+                    </div>
+                    <img src="https://getacademy.no/hs-fs/hubfs/GetAcademy-NHO_398.jpg?width=749&height=500&name=GetAcademy-NHO_398.jpg" class="eskil">
+                </div>
+            </div>
         </div>
+
         <div class="weeklySchedule_sidebar">
             <h2>Nyeste lest</h2>
             <ul>
