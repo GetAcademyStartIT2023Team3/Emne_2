0d10cef298c34f31761bbc2c6ea11014418e2a7a
Emil Bratt <emilb@getacademy.no>
2023-11-02 16:09:20 +0100

    CSS for articleView.js
    Kommet et godt stykke.
    Ikke lenge til jeg kan begynne å implementere logikk for å generere HTML for dynamisk innhold.

--- oppdrag/inside_get/implementering/src/js/articleView.js
+++ oppdrag/inside_get/implementering/src/js/articleView.js
@@ -2,72 +2,86 @@ function updateArticleView() {
     let html = '';
     html += '<div id="articlePage">';
 
-    html += renderNotes();
-    html += renderArticle();
-    html += renderNavigation();
+    html += renderLeftBox();
+    html += renderMiddleBox();
+    html += renderRightBox();
     
     html += '</div>';
 
     return html;
 }
 
-function renderNotes() {
+function renderLeftBox() {
     let html = /*html*/`
-        <div id="articlePageNotes">
-            <div style="font-size: 2rem;">
+        <div id="articlePageLeftBox">
+
+            <div id="articlePageLeftBoxTitle">
                 Notater
             </div>
-            <textarea style="width: 90%;">Notater skrives her</textarea>
+
+            <div id="articlePageLeftBoxInput">
+                <textarea>Notater skrives her</textarea>
+            </div>
+
         </div>
     `;
     return html;
 }
 
-function renderArticle() {
+function renderMiddleBox() {
     let html = /*html*/`
-        <div id="articlePageArticle">
-            <div id="articlePageArticleView">
-                <h2>Title</h2>
-                <p>
-                    Text<br>
-                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </p>
+        <div id="articlePageMiddleBox">
+
+            <div id="articlePageMiddleBoxView">
+                <div style="text-align: center;">---ARTICLE CONTENT---</div>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <div style="text-align: center;">---ARTICLE CONTENT---</div>
             </div>
-            <div>
-                <button style="display: inline;">◀</button>
-                    <div class="text">1</div>
-                    <div class="text">2</div>
-                    <div class="text">3</div>
+
+            <div id="articlePageMiddleBoxNavigation">
+                <button>◀</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
                 <button>▶</button>
             </div>
+
         </div>  
     `;
     return html;
 }
 
-function renderNavigation() {
+function renderRightBox() {
     let html = /*html*/`
-        <div id="articlePageNavigation">
-            <div>
-                <h5>Sammarbeid og kommunikasjon</h5>
-            </div>
-            <div>
-                <ul>
-                    <li><a href="">Problemløsning</a></li>
-                    <li><a href="">Læring</a></li>
-                    <li><a href="">Pomodoro</a></li>
-                </ul>
+        <div id="articlePageRightBox">
+
+            <div>Steder</div>
+
+            <div id="articlePageRightBoxTopics">
+                <div>
+                    Sammarbeid og kommunikasjon
+                    <ul>
+                        <li><a href="">Problemløsning</a></li>
+                        <li><a href="">Læring</a></li>
+                        <li><a href="">Pomodoro</a></li>
+                    </ul>
+                </div>
             </div>
 
-            <div>
-                <h5>Relaterte Artikler</h5>
+            <div id="articlePageRightBoxRelatedArticles">
+                <div>
+                    Relaterte Artikler
+                    <ul>
+                        <li><a href="">Focused/diffused mindset</a></li>
+                        <li><a href="">Pomodoro</a></li>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <ul>
-                    <li><a href="">Focused/diffused mindset</a></li>
-                    <li><a href="">Pomodoro</a></li>
-                </ul>
-            </div>
+
         </div>
     `;
     return html;
