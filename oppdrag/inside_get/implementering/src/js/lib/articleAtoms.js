7222c0cfad7484606c5d457632ddddc9c2a08827
Emil Bratt <emilb@getacademy.no>
2023-11-03 12:39:59 +0100

    Formatering

--- oppdrag/inside_get/implementering/src/js/lib/articleAtoms.js
+++ oppdrag/inside_get/implementering/src/js/lib/articleAtoms.js
@@ -1,20 +1,19 @@
 function articleAtomText() {
     return 'articleAtomText()';
-
 }
+
 function articleAtomImageHTTP() {
     return 'articleAtomImageHTTP()';
-
 }
+
 function articleAtomYoutube() {
     return 'articleAtomYoutube()';
-
 }
+
 function articleAtomImageAsset() {
     return 'articleAtomImageAsset()';
-
 }
+
 function articleAtomAskChoices() {
     return 'articleAtomAskChoices()';
-
 }
