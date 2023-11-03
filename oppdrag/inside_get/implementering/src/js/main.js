430a7703df172b9d0111ae2fd274067e98f0c96a
Emil Bratt <emilb@getacademy.no>
2023-11-03 10:29:26 +0100

    Endrer main.js
    Her har vi nå mulighet til å lage tester ved å bruke query string.
    ?test=banan => vil kalle funksjonen banan()
    Du lager "banan"-funksjonen i test.js.

--- oppdrag/inside_get/implementering/src/js/main.js
+++ oppdrag/inside_get/implementering/src/js/main.js
@@ -12,20 +12,31 @@ function main() {
     };
 
     const searchParams = new URLSearchParams(window.location.search);
-    if (searchParams.has('page')) selectPage(searchParams.get('page'));
-    if (searchParams.has('test')) test(searchParams.get('test'));
-    updateView();
+    if (searchParams.size === 0) updateView();
+    else if (searchParams.has('page')) selectPage(searchParams.get('page'));
+    else if (searchParams.has('test')) test(searchParams.get('test'));
+    else invalidQuery(searchParams);
 }
 
 
 function selectPage(page) {
     // legg til query string i URL, for eksempel "index.html?page=article" for å gå rett til article siden.
     model.app.selectedPage = page;
+    updateView();
 }
 
 
 function test(test) {
-    // legg til query string i URL, for eksempel "index.html?test=article" for å lage tester for (og se) denne siden
-    if (test === 'article') testArticle();
+    // lag en funksjon i test.js
+    // For eksempel articleTest()
+    // Du kan nå kjøre denne funksjonen ved å skrive i URL: index.html?test=articleTest
+    eval(`${test}()`);
+    updateView();
 }
 
+function invalidQuery(searchParams) {
+    let doc = document.getElementById("app");
+    for (const [key, value] of searchParams.entries()) {
+        doc.innerHTML += `${key}: ${value}`;
+    }
+}
